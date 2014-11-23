f2556650ba0688957fc374bdc4b2fe4accf7338b
John Vasileff <john@vasileff.com>
2014-11-23 17:02:49 -0500

    modules from commit 6bbde32

---
+++ modules/com/vasileff/ceylon/random/java/0.0.1/module-doc/api/.resources/index.js
@@ -0,0 +1,10 @@
+var index = [
+{'name': 'com.vasileff.ceylon.random.java', 'type': 'package', 'url': 'index.html#section-package', 'doc': '', 'tags': [],'icons': ['icon-package']},
+{'name': 'JavaRandom.reseed', 'type': 'function', 'url': 'JavaRandom.type.html#reseed', 'doc': '<p>Delegates to <code>java.util.Random.setSeed(long)</code>.</p>\n', 'tags': [],'icons': ['icon-shared-member']},
+{'name': 'JavaRandom.nextBits', 'type': 'function', 'url': 'JavaRandom.type.html#nextBits', 'doc': '<p>Generates an Integer holding <code>numBits</code> pseudorandom bits.</p>\n', 'tags': [],'icons': ['icon-shared-member', 'icon-decoration-impl']},
+{'name': 'JavaRandom', 'type': 'class', 'url': 'JavaRandom.type.html', 'doc': '<p>A pseudorandom number generator backed by <code>java.util.Random</code>.</p>\n', 'tags': [],'icons': ['icon-class', 'icon-decoration-final']},
+{'name': 'randomWhole', 'type': 'function', 'url': 'index.html#randomWhole', 'doc': '<p>Generate a pseudorandom <code><span title=\'ceylon.math.whole::Whole\'><span class=\'type-identifier\'>Whole</span></span></code> number in the range <code>origin</code>\n(inclusive) to <code>bound</code> (exclusive).</p>\n', 'tags': [],'icons': ['icon-shared-member']},
+{'name': 'randomWholeBits', 'type': 'function', 'url': 'index.html#randomWholeBits', 'doc': '<p>Generate a <code><span title=\'ceylon.math.whole::Whole\'><span class=\'type-identifier\'>Whole</span></span></code> number holding <code>numBits</code> pseudorandom bits.</p>\n', 'tags': [],'icons': ['icon-shared-member']},
+{'name': 'com.vasileff.ceylon.random.java', 'type': 'module', 'url': 'index.html', 'doc': '', 'tags': [],'icons': []}];
+var tagIndex = [
+];
---
+++ modules/com/vasileff/ceylon/random/sample/0.0.1/module-doc/api/.resources/index.js
@@ -0,0 +1,8 @@
+var index = [
+{'name': 'com.vasileff.ceylon.random.sample', 'type': 'package', 'url': 'index.html#section-package', 'doc': '', 'tags': [],'icons': ['icon-package']},
+{'name': 'LCGRandom.reseed', 'type': 'function', 'url': 'LCGRandom.type.html#reseed', 'doc': '<p>Reseed this random number generator.</p>\n', 'tags': [],'icons': ['icon-shared-member']},
+{'name': 'LCGRandom.nextBits', 'type': 'function', 'url': 'LCGRandom.type.html#nextBits', 'doc': '<p>Generates an Integer holding <code>numBits</code> pseudorandom bits.</p>\n', 'tags': [],'icons': ['icon-shared-member', 'icon-decoration-impl']},
+{'name': 'LCGRandom', 'type': 'class', 'url': 'LCGRandom.type.html', 'doc': '<p>A <a href="http://en.wikipedia.org/wiki/Linear_congruential_generator">Linear Congruential Generator</a>\n(LCG) pseudorandom number…</p>\n', 'tags': [],'icons': ['icon-class', 'icon-decoration-final']},
+{'name': 'com.vasileff.ceylon.random.sample', 'type': 'module', 'url': 'index.html', 'doc': '', 'tags': [],'icons': []}];
+var tagIndex = [
+];
