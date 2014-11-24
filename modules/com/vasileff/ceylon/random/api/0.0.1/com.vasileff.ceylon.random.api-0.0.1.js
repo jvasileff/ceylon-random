4d6cd62c6d8885fed75687b16af11a356ffafbc0
John Vasileff <john@vasileff.com>
2014-11-23 22:45:27 -0500

    updated to 1c74995 fixup randomLimits

--- modules/com/vasileff/ceylon/random/api/0.0.1/com.vasileff.ceylon.random.api-0.0.1.js
+++ modules/com/vasileff/ceylon/random/api/0.0.1/com.vasileff.ceylon.random.api-0.0.1.js
@@ -72,90 +72,89 @@ function bitLength($k){
     return $l;
 };bitLength.$crtmm$=function(){return{mod:$CCMM$,$t:{t:m$1.Integer},ps:[{nm:'number',mt:'prm',$t:{t:m$1.Integer},pa:1024}],d:['com.vasileff.ceylon.random.api','bitLength']};};
 function $o(){
-    var randomLimits$=new $o.$$;randomLimits$.$p_=m$1.getRuntime().maxIntegerValue;
-    randomLimits$.$prop$getMaxIntegerBound={$crtmm$:function(){return{mod:$CCMM$,$t:{t:m$1.Integer},$cont:$o,pa:1,an:function(){return[m$1.doc$($CCMM$,'com.vasileff.ceylon.random.api:randomLimits:$at:maxIntegerBound')];},d:['com.vasileff.ceylon.random.api','randomLimits','$at','maxIntegerBound']};}};
-    randomLimits$.$prop$getMaxIntegerBound.get=function(){return maxIntegerBound};
+    var randomLimits$=new $o.$$;randomLimits$.$prop$getMaxIntegerBound={$crtmm$:function(){return{mod:$CCMM$,$t:{t:m$1.Integer},$cont:$o,pa:3,an:function(){return[m$1.doc$($CCMM$,'com.vasileff.ceylon.random.api:randomLimits:$at:maxIntegerBound')];},d:['com.vasileff.ceylon.random.api','randomLimits','$at','maxIntegerBound']};}};
     randomLimits$.$prop$getMaxBits={$crtmm$:function(){return{mod:$CCMM$,$t:{t:m$1.Integer},$cont:$o,pa:3,an:function(){return[m$1.doc$($CCMM$,'com.vasileff.ceylon.random.api:randomLimits:$at:maxBits')];},d:['com.vasileff.ceylon.random.api','randomLimits','$at','maxBits']};}};
-    randomLimits$.$q_=m$1.getRuntime().maxIntegerValue;
-    randomLimits$.$prop$get$q={$crtmm$:function(){return{mod:$CCMM$,$t:{t:m$1.Integer},$cont:$o,pa:1024,d:['com.vasileff.ceylon.random.api','randomLimits','$at','maxValue$5s4h5b']};}};
+    randomLimits$.$p_=m$1.getRuntime().maxIntegerValue;
+    randomLimits$.$prop$get$p={$crtmm$:function(){return{mod:$CCMM$,$t:{t:m$1.Integer},$cont:$o,pa:1024,d:['com.vasileff.ceylon.random.api','randomLimits','$at','maxValue$5s4h5b']};}};
+    randomLimits$.$prop$get$p.get=function(){return $p};
+    randomLimits$.$q_=(0);
+    randomLimits$.$prop$get$q={$crtmm$:function(){return{mod:$CCMM$,$t:{t:m$1.Integer},$cont:$o,pa:1024,d:['com.vasileff.ceylon.random.api','randomLimits','$at','numBits$92hft6']};}};
     randomLimits$.$prop$get$q.get=function(){return $q};
-    randomLimits$.$r_=(0);
-    randomLimits$.$prop$get$r={$crtmm$:function(){return{mod:$CCMM$,$t:{t:m$1.Integer},$cont:$o,pa:1024,d:['com.vasileff.ceylon.random.api','randomLimits','$at','numBits$92hft6']};}};
-    randomLimits$.$prop$get$r.get=function(){return $r};
-    while((randomLimits$.$q>0)){
-        ($s=randomLimits$.$r,randomLimits$.$r=$s.successor,$s);
+    while((randomLimits$.$p>0)){
+        ($r=randomLimits$.$q,randomLimits$.$q=$r.successor,$r);
+        var $r;
+        (randomLimits$.$p=randomLimits$.$p.divided(2));
+    }
+    if(((2).power(randomLimits$.$q).minus(1)>m$1.getRuntime().maxIntegerValue)){
+        ($s=randomLimits$.$q,randomLimits$.$q=$s.predecessor,$s);
         var $s;
-        (randomLimits$.$q=randomLimits$.$q.divided(2));
     }
-    if(((2).power(randomLimits$.$r).minus(1)>m$1.getRuntime().maxIntegerValue)){
-        ($t=randomLimits$.$r,randomLimits$.$r=$t.predecessor,$t);
-        var $t;
-    }
-    var maxBits=randomLimits$.$r;
+    var maxBits=m$1.largest(randomLimits$.$q,m$1.getRuntime().integerAddressableSize,{Element$largest:{t:m$1.Integer}});
     m$1.atr$(randomLimits$,'maxBits',function(){return maxBits;},undefined,function(){return{mod:$CCMM$,$t:{t:m$1.Integer},$cont:$o,pa:3,an:function(){return[m$1.doc$($CCMM$,'com.vasileff.ceylon.random.api:randomLimits:$at:maxBits')];},d:['com.vasileff.ceylon.random.api','randomLimits','$at','maxBits']};});
+    var maxIntegerBound=m$1.smallest(m$1.getRuntime().maxIntegerValue,(2).power(m$1.smallest(63,randomLimits$.maxBits,{Element$smallest:{t:m$1.Integer}})).minus(1),{Element$smallest:{t:m$1.Integer}});
+    m$1.atr$(randomLimits$,'maxIntegerBound',function(){return maxIntegerBound;},undefined,function(){return{mod:$CCMM$,$t:{t:m$1.Integer},$cont:$o,pa:3,an:function(){return[m$1.doc$($CCMM$,'com.vasileff.ceylon.random.api:randomLimits:$at:maxIntegerBound')];},d:['com.vasileff.ceylon.random.api','randomLimits','$at','maxIntegerBound']};});
     return randomLimits$;
 };$o.$crtmm$=function(){return{mod:$CCMM$,'super':{t:m$1.Basic},pa:1,an:function(){return[m$1.doc$($CCMM$,'com.vasileff.ceylon.random.api:randomLimits'),m$1.by("John Vasileff")];},d:['com.vasileff.ceylon.random.api','randomLimits']};};
 function $init$getRandomLimits(){
     if($o.$$===undefined){
         m$1.initTypeProto($o,'com.vasileff.ceylon.random.api::randomLimits',m$1.Basic);
         (function(randomLimits$){
-            m$1.atr$(randomLimits$,'maxIntegerBound',function(){return this.$p_;},undefined,function(){return{mod:$CCMM$,$t:{t:m$1.Integer},$cont:$o,pa:1,an:function(){return[m$1.doc$($CCMM$,'com.vasileff.ceylon.random.api:randomLimits:$at:maxIntegerBound')];},d:['com.vasileff.ceylon.random.api','randomLimits','$at','maxIntegerBound']};});
-            m$1.atr$(randomLimits$,'$q',function(){return this.$q_;},function($u){return this.$q_=$u;},function(){return{mod:$CCMM$,$t:{t:m$1.Integer},$cont:$o,pa:1024,d:['com.vasileff.ceylon.random.api','randomLimits','$at','maxValue$5s4h5b']};});
-            m$1.atr$(randomLimits$,'$r',function(){return this.$r_;},function($v){return this.$r_=$v;},function(){return{mod:$CCMM$,$t:{t:m$1.Integer},$cont:$o,pa:1024,d:['com.vasileff.ceylon.random.api','randomLimits','$at','numBits$92hft6']};});
+            m$1.atr$(randomLimits$,'$p',function(){return this.$p_;},function($t){return this.$p_=$t;},function(){return{mod:$CCMM$,$t:{t:m$1.Integer},$cont:$o,pa:1024,d:['com.vasileff.ceylon.random.api','randomLimits','$at','maxValue$5s4h5b']};});
+            m$1.atr$(randomLimits$,'$q',function(){return this.$q_;},function($u){return this.$q_=$u;},function(){return{mod:$CCMM$,$t:{t:m$1.Integer},$cont:$o,pa:1024,d:['com.vasileff.ceylon.random.api','randomLimits','$at','numBits$92hft6']};});
         })($o.$$.prototype);
     }
     return $o;
 }
 ex$.$init$getRandomLimits=$init$getRandomLimits;
 $init$getRandomLimits();
-var $w;
+var $v;
 function getRandomLimits(){
-    if($w===m$1.INIT$)m$1.throwexc(m$1.InitializationError("Cyclic initialization trying to read the value of \'randomLimits\' before it was set"),'1:0-29:0','randomLimits.ceylon');
-    if($w===undefined){$w=m$1.INIT$;$w=$init$getRandomLimits()();$w.$crtmm$=getRandomLimits.$crtmm$;}
-    return $w;
+    if($v===m$1.INIT$)m$1.throwexc(m$1.InitializationError("Cyclic initialization trying to read the value of \'randomLimits\' before it was set"),'1:0-47:0','randomLimits.ceylon');
+    if($v===undefined){$v=m$1.INIT$;$v=$init$getRandomLimits()();$v.$crtmm$=getRandomLimits.$crtmm$;}
+    return $v;
 }
 ex$.getRandomLimits=getRandomLimits;
 getRandomLimits.$crtmm$=function(){return{mod:$CCMM$,$t:{t:$o},pa:1,an:function(){return[m$1.doc$($CCMM$,'com.vasileff.ceylon.random.api:randomLimits'),m$1.by(["John Vasileff"].rt$({t:m$1.$_String}))];},d:['com.vasileff.ceylon.random.api','randomLimits']};};
 $prop$getRandomLimits={get:getRandomLimits,$crtmm$:getRandomLimits.$crtmm$};
 getRandomLimits=getRandomLimits;$prop$getRandomLimits=getRandomLimits;
 ex$.$prop$getRandomLimits=$prop$getRandomLimits;
-function stream($x,$$$mptypes){
-    var $y=m$1.JsCallable(0,$x);
-    function $z($$targs$$){
-        var $z$=new $z.$$;$z$.$$targs$$=$$targs$$;
-        m$1.Iterable({Absent$Iterable:{t:m$1.Nothing},Element$Iterable:$$$mptypes.Element$stream},$z$);
-        m$1.Iterator({Element$Iterator:$$$mptypes.Element$stream},$z$);
-        return $z$;
-    };$z.$crtmm$=function(){return{mod:$CCMM$,'super':{t:m$1.Basic},$cont:stream,sts:[{t:m$1.Iterable,a:{Absent$Iterable:{t:m$1.Nothing},Element$Iterable:'Element$stream'}},{t:m$1.Iterator,a:{Element$Iterator:'Element$stream'}}],d:['com.vasileff.ceylon.random.api','stream','$o','it$d7hs1l']};};
-    function $init$$z(){
-        if($z.$$===undefined){
-            m$1.initTypeProto($z,'com.vasileff.ceylon.random.api::it',m$1.Basic,m$1.Iterator,m$1.Iterable);
-            (function($z$){
-                $z$.iterator=function(){
-                    var $z$=this;
-                    return $z$;
+function stream($w,$$$mptypes){
+    var $x=m$1.JsCallable(0,$w);
+    function $y($$targs$$){
+        var $y$=new $y.$$;$y$.$$targs$$=$$targs$$;
+        m$1.Iterable({Absent$Iterable:{t:m$1.Nothing},Element$Iterable:$$$mptypes.Element$stream},$y$);
+        m$1.Iterator({Element$Iterator:$$$mptypes.Element$stream},$y$);
+        return $y$;
+    };$y.$crtmm$=function(){return{mod:$CCMM$,'super':{t:m$1.Basic},$cont:stream,sts:[{t:m$1.Iterable,a:{Absent$Iterable:{t:m$1.Nothing},Element$Iterable:'Element$stream'}},{t:m$1.Iterator,a:{Element$Iterator:'Element$stream'}}],d:['com.vasileff.ceylon.random.api','stream','$o','it$d7hs1l']};};
+    function $init$$y(){
+        if($y.$$===undefined){
+            m$1.initTypeProto($y,'com.vasileff.ceylon.random.api::it',m$1.Basic,m$1.Iterator,m$1.Iterable);
+            (function($y$){
+                $y$.iterator=function(){
+                    var $y$=this;
+                    return $y$;
                 };
-                $z$.iterator.$crtmm$=function(){return{mod:$CCMM$,$t:{t:m$1.Iterator,a:{Element$Iterator:'Element$stream'}},ps:[],$cont:$z,pa:3,d:['com.vasileff.ceylon.random.api','stream','$o','it$d7hs1l','$m','iterator']};};
-                $z$.next=function(){
-                    var $z$=this;
-                    return $y();
+                $y$.iterator.$crtmm$=function(){return{mod:$CCMM$,$t:{t:m$1.Iterator,a:{Element$Iterator:'Element$stream'}},ps:[],$cont:$y,pa:3,d:['com.vasileff.ceylon.random.api','stream','$o','it$d7hs1l','$m','iterator']};};
+                $y$.next=function(){
+                    var $y$=this;
+                    return $x();
                 };
-                $z$.next.$crtmm$=function(){return{mod:$CCMM$,$t:'Element$stream',ps:[],$cont:$z,pa:3,d:['com.vasileff.ceylon.random.api','stream','$o','it$d7hs1l','$m','next']};};
-            })($z.$$.prototype);
+                $y$.next.$crtmm$=function(){return{mod:$CCMM$,$t:'Element$stream',ps:[],$cont:$y,pa:3,d:['com.vasileff.ceylon.random.api','stream','$o','it$d7hs1l','$m','next']};};
+            })($y.$$.prototype);
         }
+        return $y;
+    }
+    $init$$y();
+    var $z;
+    function get$z(){
+        if($z===m$1.INIT$)m$1.throwexc(m$1.InitializationError("Cyclic initialization trying to read the value of \'it\' before it was set"),'8:2-11:2','stream.ceylon');
+        if($z===undefined){$z=m$1.INIT$;$z=$init$$y()({Absent$Iterable:{t:m$1.Nothing},Element$Iterator:$$$mptypes.Element$stream,Element$Iterable:$$$mptypes.Element$stream});$z.$crtmm$=get$z.$crtmm$;}
         return $z;
     }
-    $init$$z();
-    var $10;
-    function get$10(){
-        if($10===m$1.INIT$)m$1.throwexc(m$1.InitializationError("Cyclic initialization trying to read the value of \'it\' before it was set"),'8:2-11:2','stream.ceylon');
-        if($10===undefined){$10=m$1.INIT$;$10=$init$$z()({Absent$Iterable:{t:m$1.Nothing},Element$Iterator:$$$mptypes.Element$stream,Element$Iterable:$$$mptypes.Element$stream});$10.$crtmm$=get$10.$crtmm$;}
-        return $10;
-    }
-    get$10.$crtmm$=function(){return{mod:$CCMM$,$t:{t:$z},$cont:stream,d:['com.vasileff.ceylon.random.api','stream','$o','it$d7hs1k']};};
-    $prop$get$10={get:get$10,$crtmm$:get$10.$crtmm$};
-    get$z=get$10;$prop$get$z=get$10;
-    return get$10();
+    get$z.$crtmm$=function(){return{mod:$CCMM$,$t:{t:$y},$cont:stream,d:['com.vasileff.ceylon.random.api','stream','$o','it$d7hs1k']};};
+    $prop$get$z={get:get$z,$crtmm$:get$z.$crtmm$};
+    get$y=get$z;$prop$get$y=get$z;
+    return get$z();
 }
 ex$.stream=stream;
 stream.$crtmm$=function(){return{mod:$CCMM$,$t:{t:m$1.Iterable,a:{Absent$Iterable:{t:m$1.Nothing},Element$Iterable:'Element$stream'}},ps:[{nm:'next',mt:'prm',$pt:'f',$t:'Element$stream',an:function(){return[m$1.doc$($CCMM$,'com.vasileff.ceylon.random.api:stream:$m:next$7vlz3x')];}}],tp:{Element$stream:{}},pa:1,an:function(){return[m$1.doc$($CCMM$,'com.vasileff.ceylon.random.api:stream'),m$1.by(["John Vasileff"].rt$({t:m$1.$_String}))];},d:['com.vasileff.ceylon.random.api','stream']};};
